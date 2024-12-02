893b18149e2b4f35f730acd83816303a9a6a0053
SlimJesus25 <1210828@isep.ipp.pt>
2024-12-02 13:27:26 +0000

    .

--- vbox_shared/Caso_Pratico/utils.h
+++ vbox_shared/Caso_Pratico/utils.h
@@ -10,24 +10,32 @@ typedef struct{
 }timestamp_t;
 
 void log_msg(char* message){
+    if(message == NULL)
+        return;
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    printf("%d-%02d-%02d %02d:%02d:%02d %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, message);
+    printf("[Thread-%ld] %d-%02d-%02d %02d:%02d:%02d %s\n", pthread_self(), tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, message);
 }
 
 void log_error(char* message){
+    if(message == NULL)
+        return;
     char msg[strlen(message) + strlen("[ERROR] ") + 1];
     sprintf(msg, "[ERROR] %s", message);
     log_msg(msg);
 }
 
 void log_info(char* message){
+    if(message == NULL)
+        return;
     char msg[strlen(message) + strlen("[INFO] ") + 1];
     sprintf(msg, "[INFO] %s", message);
     log_msg(msg);
 }
 
 void log_warning(char* message){
+    if(message == NULL)
+        return;
     char msg[strlen(message) + strlen("[WARNING] ") + 1];
     sprintf(msg, "[WARNING] %s", message);
     log_msg(msg);
@@ -37,13 +45,13 @@ void decode_date_observed(char* date_observed, timestamp_t* ts){
     char h[3], m[3], s[3], ms[3];
 
     strncpy(h, date_observed+11, 2);
-    h[3] = '\0';
+    h[2] = '\0';
     strncpy(m, date_observed+14, 2);
-    m[3] = '\0';
+    m[2] = '\0';
     strncpy(s, date_observed+17, 2);
-    s[3] = '\0';
+    s[2] = '\0';
     strncpy(ms, date_observed+20, 2);
-    ms[3] = '\0';
+    ms[2] = '\0';
 
     ts->hour = atoi(h);
     ts->minute = atoi(m);
